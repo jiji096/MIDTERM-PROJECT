962e961531782267c2b5279643bedcd8145c4811
jiji096 <janelle.angela.lopez@student.ateneo.edu>
2025-02-24 18:23:03 +0800

    ITS MOVINGGG

--- SceneFrame.java
+++ SceneFrame.java
@@ -2,7 +2,7 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class SceneFrame extends JComponent { 
+public class SceneFrame extends JComponent implements ActionListener { 
     private JFrame frame;
     private JButton rotate, clockwise;
     private SceneCanvas scene;
@@ -11,6 +11,7 @@ public class SceneFrame extends JComponent {
     private int state1 = 0;
     private int state2 = 0;
     private int day = 0;
+    private Timer loopie;
     
     public SceneFrame(int w, int h){
         frame = new JFrame();
@@ -19,10 +20,10 @@ public class SceneFrame extends JComponent {
        
         rotate = new JButton("rotate");
         clockwise = new JButton("clockwise");
-
         scene = new SceneCanvas(w, h);
-    }
 
+        loopie = new Timer(100, this);
+    }
 
     public void setUpGUI(){
         Container contentPane = frame.getContentPane();
@@ -36,62 +37,128 @@ public class SceneFrame extends JComponent {
         contentPane.add(scene, "Center");
         contentPane.add(buttons, BorderLayout.SOUTH);
 
-        frame.setTitle("ERMMMM");
-        //frame.add(scene);
+        frame.setTitle("Midterm Project - Lopez - Nellas");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setFocusable(true);
         frame.setVisible(true);
+
+        rotate.addActionListener(this);
+        clockwise.addActionListener(this);
     }
 
-    public void setUpButtonListener(){
-        ActionListener buttonListener = new ActionListener(){
-            public void actionPerformed(ActionEvent x){
-                JButton b = (JButton) x.getSource();
-                Dolphin d = scene.getDolphin();
-                RecordPlayer r = scene.getRecordPlayer();
-                Fish1 f1 = scene.getFish1();
-                Rainbow rb = scene.getRainbow();
-                Sun sun = scene.getSun();
+    @Override
+    public void actionPerformed(ActionEvent e){
+        //JButton b = (JButton) e.getSource();
 
+        if (e.getSource() == loopie){
+            update();
+        }
 
-    
-                if (b == rotate){
-                    d.rotate();
-                    sun.rotate();
-                    f1.moveLeft();
-                    day+=5;
-                    if (day > 60 && day < 270) {
-                        rb.setTransparency(0);
-                    }
-                    if (day <= 30 || day >= 270) {
-                        state2 = 0;
-                    }
-                    else {
-                        state2 = 1;
-                    }
-                    if (day == 360) {
-                        day = 0;
-                    }
-                    if (day > 270 && day < 360) {
-                        rb.turnOpaque();
-                    }
-                    else {
-                        rb.turnTransparent();
-                    }
-                    System.out.println(rb.getTransparency());
-                    System.out.println("day: " + day);
-                }
-
-                else if (b == clockwise){
-                    d.rotateCC();
-                }
-
-                scene.repaint();
+        else if (e.getSource() == rotate){
+            if(!loopie.isRunning()){
+                loopie.start();
             }
-            
-        };
-        rotate.addActionListener(buttonListener);
-        clockwise.addActionListener(buttonListener);
+        }
+
+        else if (e.getSource() == clockwise){
+            if(loopie.isRunning()){
+                loopie.stop();
+            }
+        }
+    }
+
+    private void update(){
+        Dolphin d = scene.getDolphin();
+        RecordPlayer r = scene.getRecordPlayer();
+        Fish1 f1 = scene.getFish1();
+        Rainbow rb = scene.getRainbow();
+        Sun sun = scene.getSun();
+
+        d.rotate();
+        sun.rotate();
+        f1.moveLeft();
+        day+=5;
+        if (day > 60 && day < 270) {
+            rb.setTransparency(0);
+        }
+        if (day <= 30 || day >= 270) {
+            state2 = 0;
+        }
+        else {
+            state2 = 1;
+        }
+        if (day == 360) {
+            day = 0;
+        }
+        if (day > 270 && day < 360) {
+            rb.turnOpaque();
+        }
+        else {
+            rb.turnTransparent();
+        }
+        System.out.println(rb.getTransparency());
+        System.out.println("day: " + day);
+
+        scene.repaint();
 
     }
 }
+    
+    // public void setUpButtonListener(){
+    //     ActionListener buttonListener = new ActionListener(){
+    //         loopie = new Timer(10, buttonListener);
+
+    //             public void actionPerformed(ActionEvent x){
+
+    //                 JButton b = (JButton) x.getSource();
+    //                 Dolphin d = scene.getDolphin();
+    //                 RecordPlayer r = scene.getRecordPlayer();
+    //                 Fish1 f1 = scene.getFish1();
+    //                 Rainbow rb = scene.getRainbow();
+    //                 Sun sun = scene.getSun();
+        
+    //                 System.out.println("hey loop");
+    
+    //                 if (b == rotate){
+                        
+    //                     // scene.run();
+    //                     d.rotate();
+    //                     sun.rotate();
+    //                     f1.moveLeft();
+    //                     day+=5;
+    //                     if (day > 60 && day < 270) {
+    //                         rb.setTransparency(0);
+    //                     }
+    //                     if (day <= 30 || day >= 270) {
+    //                         state2 = 0;
+    //                     }
+    //                     else {
+    //                         state2 = 1;
+    //                     }
+    //                     if (day == 360) {
+    //                         day = 0;
+    //                     }
+    //                     if (day > 270 && day < 360) {
+    //                         rb.turnOpaque();
+    //                     }
+    //                     else {
+    //                         rb.turnTransparent();
+    //                     }
+    //                     System.out.println(rb.getTransparency());
+    //                     System.out.println("day: " + day);
+    //                 }    
+    
+    //                 // else if (b == clockwise){
+    //                 //     d.rotateCC();
+    //                 // }
+    
+    //                 scene.repaint();
+                    
+    //             }
+            
+    //     }
+    //     rotate.addActionListener(buttonListener);
+    //     clockwise.addActionListener(buttonListener);
+
+    // }
