209d588daf1c592783b104f643260bcdb75bdb55
Ajtn05 <68014277+Ajtn05@users.noreply.github.com>
2025-02-28 02:09:51 +0800

    shortened code

--- SceneCanvas.java
+++ SceneCanvas.java
@@ -5,63 +5,47 @@ import javax.swing.*;
 public class SceneCanvas extends JComponent { 
     private int width;
     private int height;
-    private Sun mrSun;
-    private Triangle rays;
-    private Waves dagat;
     private Dolphin dolphin;
     private Fish1 fish1;
-    private Chest chest;
     private RecordPlayer record;
+    private Sun mrSun;
     private Rainbow rainbow;
     private Sky sky;
-    private Cloud cloud1, cloud2, cloud3, cloud4, cloud5, cloud6;
-    private Coral coral1, coral2, coral3;
     private Stars stars;
+    
     private ArrayList<DrawingObject> elements = new ArrayList<>();
     
     public SceneCanvas(int w, int h){
         width = w;
         height = h;
-
-        dolphin = new Dolphin(400,1100);
-        mrSun = new Sun();
         
         Color blue = new Color(100,149,237);
         Color darkBlue = new Color(	66, 117, 209);
-        cloud1 = new Cloud(105, 70, 70, darkBlue);
-        cloud2 = new Cloud(100,65,70,blue);
-        cloud3 = new Cloud(5, 80, 70, darkBlue);
-        cloud4 = new Cloud(0,75,70,blue);
-        cloud5 = new Cloud(525, 85, 70, darkBlue);
-        cloud6 = new Cloud(520,80,70,blue);
-
+        
+        dolphin = new Dolphin(400,1100);
         fish1 = new Fish1();
-        dagat = new Waves();
-        chest = new Chest();
         record = new RecordPlayer(707.33,523);
+        mrSun = new Sun();
         rainbow = new Rainbow();
-        coral1 = new Coral(0);
-        coral2 = new Coral(300);
-
         sky = new Sky(400, 0, new Color( 176, 206, 255), 400, 600, new Color (59, 0, 100 ));
         stars = new Stars(0, 0);
 
         elements.add(sky);
         elements.add(stars);
         elements.add(rainbow);
-        elements.add(dagat);
+        elements.add(new Waves());
         elements.add(dolphin);
         elements.add(mrSun);
-        elements.add(cloud1);
-        elements.add(cloud2);
-        elements.add(cloud3);
-        elements.add(cloud4);
-        elements.add(cloud5);
-        elements.add(cloud6);
+        elements.add(new Cloud(105, 70, 70, darkBlue));
+        elements.add(new Cloud(100,65,70,blue));
+        elements.add(new Cloud(5, 80, 70, darkBlue));
+        elements.add(new Cloud(0,75,70,blue));
+        elements.add(new Cloud(525, 85, 70, darkBlue));
+        elements.add(new Cloud(520,80,70,blue));
         elements.add(fish1);
         elements.add(record);
-        elements.add(coral1);
-        elements.add(coral2);
+        elements.add(new Coral(0));
+        elements.add(new Coral(300));
     }
 
     @Override
@@ -78,25 +62,6 @@ public class SceneCanvas extends JComponent {
             elements.get(i).draw(g2d);
         }
         
-        // sky.draw(g2d);
-        // mrSun.draw(g2d);
-
-        // for (int i = 0; i < clouds.size(); i++) {
-        //     clouds.get(i).draw(g2d);
-        // }
-
-        // dolphin.draw(g2d);
-        // dagat.draw(g2d);
-        // dolphin.draw(g2d);
-        // fish1.draw(g2d);
-        // chest.draw(g2d);
-        // record.draw(g2d); 
-
-
-        // if (fish1.getX() == 600) {
-        //     fish1.draw(g2d);
-        // }
-
 
 
     }  
